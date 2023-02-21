c504f878589698637fa44fca819b1fb6efee7a8c
Thiago <thiag@NOTEBOOKTH>
2023-02-21 09:41:10 -0300

    redis

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+
+using System.Text.Json;
+
+
+IDistributedCache _cache = new IDistributedCache();
+
+var cacheKey = "Clientes";
+string json = string.Empty;
+json = JsonSerializer.Serialize<List<string>>(null);
+await _cache.SetStringAsync(cacheKey, json);
+
+await _cache.SetStringAsync(cacheKey, json);
